2f00f199c1f76b74a5a48252df70392f90c7ca48
bi0punk <systecinfo_cl@criptext.com>
2024-01-15 16:58:44 -0300

    view data

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pyplot
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+
+
+
+
+df = pd.read_csv('titanic/train.csv')
+print(df.info())
+
+
+
+df = df.drop(['PassengerId', 'Ticket', 'Cabin'],axis = 1 )
+print(df.heead(10))
