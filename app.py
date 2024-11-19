bd7ac85ed49b2fd885032cbf95c61feb67b3406f
enrique <74167958+bi0punk@users.noreply.github.com>
2024-11-18 23:31:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,8 @@ import matplotlib.pyplot as pyplot
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 
-
-
-
 df = pd.read_csv('titanic/train.csv')
 print(df.info())
-
-
 
 df = df.drop(['PassengerId', 'Ticket', 'Cabin'],axis =1)
 print(df.head(10))
